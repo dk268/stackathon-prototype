ad1bfe3b2bc65d4e73ffacac3c0ac4f56a911fb3
dk268 <lorddanp@aol.com>
2018-06-12 00:31:23 -0400

    Refactor: reverted TableCheckpoints to stateful class component

--- client/components/TableCheckpoints.jsx
+++ client/components/TableCheckpoints.jsx
@@ -6,54 +6,61 @@ import Loading from "./Loading";
 import Error from "./Error";
 import { getCheckpoints } from "../reducers/allCheckpoints";
 
-const TableCheckpoints = props => {
-  console.log(props);
-  const filteredCheckpoints = props.allCheckpoints
-    .filter(checkpoint => checkpoint.raidId === props.raid.id)
-    .filter(
-      checkpoint =>
-        checkpoint.characters.filter(
-          character => character.id === props.character.id
-        ).length
-    );
-  switch (props.status) {
-    case UNASKED:
-      return <Unasked />;
-    case ERROR:
-      return <Error componentName="TableRaids" />;
-    case LOADING: {
-      return <Loading name="table of raids" />;
-    }
-    case LOADED: {
-      return (
-        <div className="table-checkpoints-div">
-          {filteredCheckpoints.map(checkpoint => (
-            <p key={checkpoint.id}>
-              {checkpoint.checkpointName} for {checkpoint.checkpointDKP} dkp
-            </p>
-          ))}
-          <p>
-            Total DKP{" "}
-            {filteredCheckpoints.reduce(
-              (acc, checkpoint) => acc + checkpoint.checkpointDKP,
-              0
-            )}
-          </p>
-        </div>
+class TableCheckpoints extends Component {
+  componentDidMount = () => {
+    this.props.getCheckpoints();
+  };
+
+  render = () => {
+    console.log(this.props);
+    const filteredCheckpoints = this.props.allCheckpoints
+      .filter(checkpoint => checkpoint.raidId === this.props.raid.id)
+      .filter(
+        checkpoint =>
+          checkpoint.characters.filter(
+            character => character.id === this.props.character.id
+          ).length
       );
+    switch (this.props.status) {
+      case UNASKED:
+        return <Unasked />;
+      case ERROR:
+        return <Error componentName="TableRaids" />;
+      case LOADING: {
+        return <Loading name="table of raids" />;
+      }
+      case LOADED: {
+        return (
+          <div className="table-checkpoints-div">
+            {filteredCheckpoints.map(checkpoint => (
+              <p key={checkpoint.id}>
+                {checkpoint.checkpointName} for {checkpoint.checkpointDKP} dkp
+              </p>
+            ))}
+            <p>
+              Total DKP{" "}
+              {filteredCheckpoints.reduce(
+                (acc, checkpoint) => acc + checkpoint.checkpointDKP,
+                0
+              )}
+            </p>
+          </div>
+        );
+      }
+      default: {
+        return <Unasked />;
+      }
     }
-    default: {
-      props.getCheckpoints();
-      return <Unasked />;
-    }
-  }
-};
+  };
+}
+
 const mapStateToProps = state => ({
   status: state.allCheckpoints.status,
   allCheckpoints: state.allCheckpoints.collection,
 });
 
 const mapDispatchToProps = { getCheckpoints };
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
