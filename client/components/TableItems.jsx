4136c0f5a9a46c56eba37e0fcca8c1da6d750d2d
dk268 <lorddanp@aol.com>
2018-06-12 20:18:38 -0400

    Infrastructure: makes TableItems a supercomponent returning a render based on props

--- client/components/TableItems.jsx
+++ client/components/TableItems.jsx
@@ -9,46 +9,70 @@ import { Link } from "react-router-dom";
 
 class TableItems extends Component {
   componentDidMount = () => {
-    this.props.getItems();
+    if (this.props.character) this.props.getItems();
   };
-  render = () => {
-    switch (this.props.status) {
-      case UNASKED:
-        return <Unasked />;
-      case ERROR:
-        return <Error componentName="TableItems" />;
-      case LOADING:
-        return <Loading name="table of items" />;
-      case LOADED: {
-        const filteredItems = this.props.allItems.filter(
-          item => item.character.id === this.props.character.id
-        );
-        return (
-          <div id="table-raids-div">
-            {filteredItems.map(item => (
-              <p key={item.id}>
-                name: <Link to={`/items/${item.id}`}>{item.itemName}</Link>{" "}
-                raid:{" "}
-                <Link to={`/raids/${item.RaidAcquired.id}`}>
-                  {item.RaidAcquired.raidName}
-                </Link>{" "}
-                for {item.itemDKPCost} dkp
-              </p>
-            ))}
-          </div>
-        );
-      }
-      default:
-        return <Unasked />;
+
+  TableItemsCharacter = props => {
+    {
+      const filteredItems = props.allItems.filter(
+        item => item.character.id === props.character.id
+      );
+      return (
+        <div id="table-raids-div">
+          {filteredItems.map(item => (
+            <p key={item.id}>
+              name: <Link to={`/items/${item.id}`}>{item.itemName}</Link> raid:{" "}
+              <Link to={`/raids/${item.RaidAcquired.id}`}>
+                {item.RaidAcquired.raidName}
+              </Link>{" "}
+              for {item.itemDKPCost} dkp
+            </p>
+          ))}
+        </div>
+      );
     }
+  };
+
+  TableItemsRaid = props => {
+    {
+      return (
+        <div id="table-raids-div">
+          {props.raid.items.map(item => (
+            <p key={item.id}>
+              name: <Link to={`/items/${item.id}`}>{item.itemName}</Link> for{" "}
+              {item.itemDKPCost} dkp
+            </p>
+          ))}
+        </div>
+      );
+    }
+  };
+
+  render = () => {
+    if (this.props.raid) return this.TableItemsRaid(this.props);
+    else
+      switch (this.props.status) {
+        case UNASKED:
+          return <Unasked />;
+        case ERROR:
+          return <Error componentName="TableItems" />;
+        case LOADING:
+          return <Loading name="table of items" />;
+        case LOADED: {
+          return this.TableItemsCharacter(this.props);
+        }
+        default:
+          return <Unasked />;
+      }
   };
 }
 
-const mapStateToProps = state => ({
-  status: state.allItems.status,
-  allItems: state.allItems.collection,
-});
-
+const mapStateToProps = state => {
+  return {
+    status: state.allItems.status,
+    allItems: state.allItems.collection,
+  };
+};
 const mapDispatchToProps = { getItems };
 
 export default connect(
