c83922de7c8e60d35895608e2eeda7d49a81a1fd
dk268 <lorddanp@aol.com>
2018-06-15 20:53:45 -0400

    Fix: fixes alt radio buttons to work

--- client/components/FormCharacter.jsx
+++ client/components/FormCharacter.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const FormCharacter = ownProps => {
-  console.log(ownProps);
   return (
     <form onSubmit={ownProps.handleSubmit}>
       Character Name: <br />
