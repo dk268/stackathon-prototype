b5e2fc0e84904081e1ffc7f58b5c4be99b887d7a
Danny <Lorddanp@aol.com>
2018-06-17 17:23:13 -0400

    Feat. Attempts to start Material UI

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import "../public/index.css";
 import store from "./store.js";
 import Root from "./components/root.jsx";
+import "typeface-roboto";
 import { LOGGED_IN_AUTH, ADMIN_AUTH } from "./reducers/auth";
 
 render(
