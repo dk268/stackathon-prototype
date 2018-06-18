8b8bbad43f23a9f27b8a28c04dd668ef51c28fd3
dk268 <32686586+dk268@users.noreply.github.com>
2018-06-17 22:43:15 -0400

    Revert "Feature"

--- client/components/RowCheckpoint.jsx
+++ client/components/RowCheckpoint.jsx
@@ -31,18 +31,17 @@ export default RowCheckpoint;
 const RowCheckpointCheckpoints = props => (
   <div id="checkpoint-checkpoint-outer-div">
     <ul className="checkpoint-detail-map-item">
-      <li className="checkpoint-detail-raid-li">
-        belongs to raid:{!props.checkpoint.raid ? (
-          ` Checkpoint does not belong to a raid`
-        ) : (
-          <Link to={`/raids/${props.checkpoint.raid.id}`}>
-            {props.checkpoint.raid.raidName}
-          </Link>
-        )}
-      </li>
+      <li className="checkpoint-detail-name-li">
+        {props.checkpoint.checkpointName}
+      </li>{" "}
       <li className="checkpoint-detail-dkp-li">
         {"dkp value: "}
         {props.checkpoint.checkpointDKP}
+      </li>
+      <li className="checkpoint-detail-raid-li">
+        <Link to={`/raids/${props.checkpoint.raid.id}`}>
+          {props.checkpoint.raid.raidName}
+        </Link>
       </li>
     </ul>
   </div>
