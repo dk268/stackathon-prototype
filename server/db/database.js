101900f3b6ba40fbf738115b228bcb02fce28fb8
dk268 <lorddanp@aol.com>
2018-06-18 12:30:39 -0400

    Fix: adds varaible for use in heroku deployment

--- server/db/database.js
+++ server/db/database.js
@@ -11,13 +11,15 @@ const pkg = require("../../package.json");
 console.log(chalk.yellow("Opening database connection"));
 console.log(chalk.blue(`process.env.SHELL: ${process.env.SHELL}`));
 
+const currentDBUrl = `postgres://sctvjqlfeoszpk:c027269f585280bf0cbf45c694eed4f0b99b6fa44edc9861fd567bdb6b146093@ec2-54-235-132-202.compute-1.amazonaws.com:5432/daht5bo9gk4gq9`;
 // create the database instance that can be used in other database files
 const db = new Sequelize(
-  process.env.SHELL === "C:\\Program Files\\Git\\usr\\bin\\bash.exe"
-    ? `postgres://postgres:moop@localhost:5432/${pkg.name}`
-    : `postgres://localhost:5432/${pkg.name}`,
+  // process.env.SHELL === "C:\\Program Files\\Git\\usr\\bin\\bash.exe"
+  //   ? `postgres://postgres:moop@localhost:5432/${pkg.name}`
+  //   : `postgres://localhost:5432/${pkg.name}`
+  currentDBUrl,
   {
-    logging: false // so we don't see all the SQL queries getting made
+    logging: false, // so we don't see all the SQL queries getting made
   }
 );
 /* //for Heroku
